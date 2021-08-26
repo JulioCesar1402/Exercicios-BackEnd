8614b67c06094208106ae3dc298c28eded8614d1
JulioCesar1402 <julio.cesarcj1402@gmail.com>
2021-08-26 08:43:47 -0300

    left-right-inner

--- MySQL/Exercises/EX7.sql
+++ MySQL/Exercises/EX7.sql
@@ -73,5 +73,40 @@ Select * from sakila.actor;
 Select * from sakila.film_actor;
 Select * from sakila.film;
 
-
 -- # ====================================================================
+
+  SELECT
+      c.customer_id,
+      c.first_name,
+      c.last_name,
+      a.actor_id,
+      a.first_name,
+      a.last_name
+  FROM sakila.customer AS c
+  LEFT JOIN sakila.actor AS a
+  ON c.last_name = a.last_name
+  ORDER BY c.last_name;
+
+  SELECT
+    c.customer_id,
+    c.first_name,
+    c.last_name,
+    a.actor_id,
+    a.first_name,
+    a.last_name
+FROM sakila.customer AS c
+RIGHT JOIN sakila.actor AS a
+ON c.last_name = a.last_name
+ORDER BY c.last_name;
+
+SELECT
+    c.customer_id,
+    c.first_name,
+    c.last_name,
+    a.actor_id,
+    a.first_name,
+    a.last_name
+FROM sakila.customer AS c
+INNER JOIN sakila.actor AS a
+ON c.last_name = a.last_name
+ORDER BY c.last_name;
