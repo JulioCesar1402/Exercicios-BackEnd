cec44a207002d764ff360bf273a320e8482020ef
JulioCesar1402 <julio.cesarcj1402@gmail.com>
2021-08-27 16:37:59 -0300

    fixaçao concluida

--- MySQL/Exercises/EX8.sql
+++ MySQL/Exercises/EX8.sql
@@ -330,5 +330,43 @@ DELIMITER ;
 DELETE FROM carros WHERE id_carro = 1;
 SELECT * FROM log_operacoes;
 
+-- # =======================================================================
+-- * Crie um Trigger para INSERT que deve definir o valor do campo release_year da tabela movies como o ano atual de forma dinâmica, sem haver a necessidade de digitar manualmente o valor do ano. Além disso, crie um outro Trigger para INSERT que adiciona um novo registro na tabela movies_logs , informando o movie_id do filme que acaba de ser inserido na tabela movies , a executed_action como 'INSERT' e a log_date como a data atual.
+
+USE BeeMovies;
+DELIMITER $$
+
+CREATE TRIGGER trigger_insert_year
+    BEFORE INSERT ON movies
+    FOR EACH ROW
+BEGIN
+    SET NEW.release_year = YEAR(NOW());
+END $$
+
+CREATE TRIGGER trigger_insert_valor
+    AFTER INSERT ON movies
+    FOR EACH ROW
+BEGIN
+    INSERT INTO movies_logs(movie_id, executed_action, log_date)
+    VALUES(NEW.movie_id, 'INSERT', NOW());
+END $$
+DELIMITER ;
+
+INSERT INTO movies(ticket_price) VALUES (0);
+
+-- * Crie um Trigger para UPDATE que, ao receber uma alteração na tabela movies , deve comparar o valor anterior de ticket_price com o valor sendo inserido nesta atualização. Caso o valor seja maior que o anterior, insira na coluna ticket_price_estimation o valor de 'Increasing' . Caso contrário, insira o valor 'Decreasing' . Adicionalmente, insira um novo registro na tabela movies_logs , contendo informações sobre o registro alterado ( movie_id , executed_action e log_date ).
+
+
+
+-- * Crie um Trigger na tabela movies que, ao ter algum de seus registros excluídos, deve enviar uma informação para a tabela movies_logs , onde devem ser guardados a data da exclusão, a executed_action 'DELETE' e o id do filme excluído.
+
+
+
+
+
+
+
+
+
 
 -- # =======================================================================
